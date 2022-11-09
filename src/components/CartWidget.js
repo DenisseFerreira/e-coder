23364d7c7ec4435199d9ef5a05c8919a3f7303ad
DenisseFerreira <ferreira.denisse@gmail.com>
2022-11-09 19:38:26 -0300

    componentes y props

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+import carritoNavbar from "../assets/carritoNavbar.png";
+const CartWidget = () => {
+  return (
+    <button>
+        <img src={carritoNavbar}/>
+    </button>
+  )
+}
+
+export default CartWidget;
